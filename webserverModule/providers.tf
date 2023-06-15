877e6fdf8b53d0cf69b354dd11059218e360a76c
Samir <samir@lafabriqueaclients.fr>
2023-06-15 16:25:22 +0200

    cred

--- webserverModule/providers.tf
+++ webserverModule/providers.tf
@@ -6,10 +6,10 @@ terraform {
 }
 
 provider "azurerm" {
-   subscription_id = var.subscription_id
-   #client_id = var.client_id
-   #client_secret = var.client_secret
-   tenant_id = var.tenant_id
+   subscription_id = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
+   client_id = "2ac719ef-3f8b-45ad-b133-8d66e0919476"
+   client_secret = "9rf8Q~iPU1Ur5fvNpXk.Z1kl99-27F16mygVNcLG"
+   tenant_id = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
    skip_provider_registration = true
    features {}
 }
