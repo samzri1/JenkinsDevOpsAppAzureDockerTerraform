5e1d237693df9ece80021f2dcc26ec59366ec85b
obabaldbiyat <127845320+obabaldbiyat@users.noreply.github.com>
2023-06-15 11:44:48 +0200

    Update main.tf

--- webserverModule/main.tf
+++ webserverModule/main.tf
@@ -26,6 +26,17 @@ resource "azurerm_network_security_group" "allowedports" {
        source_address_prefix = "*"
        destination_address_prefix = "*"
    }
+     security_rule {
+       name = "http"
+       priority = 1070
+       direction = "Inbound"
+       access = "Allow"
+       protocol = "Tcp"
+       source_port_range = "*"
+       destination_port_range = "1234"
+       source_address_prefix = "*"
+       destination_address_prefix = "*"
+   }
 
    security_rule {
        name = "https"
